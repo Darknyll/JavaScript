daf482bb70dcab0999128e1d8dc0f4e9d7e8d6c8
Darknyll <vivanskiy0@gmail.com>
2022-07-21 23:12:54 +0300

    lesson1

---
+++ lesson1/JS/main.js
@@ -0,0 +1,76 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+console.log(a, b, c, d, e, f, g, h, i, j, k, l, m);
+
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(i);
+alert(j);
+alert(k);
+alert(l);
+alert(m);
+alert(a+b+c+d+e+f+j+h+i+j+k+l+m);
+
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(f);
+document.write(g);
+document.write(h);
+document.write(i);
+document.write(j);
+document.write(k);
+document.write(l);
+document.write(m);
+
+let firstName = "Іванський";
+let middleName = "Віталій";
+let lastName = "Богданович";
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let n = 100;
+let o = '100';
+let p = true;
+
+console.log(typeof n);
+console.log(typeof o);
+console.log(typeof p);
+
+prompt(firstName);
+prompt(middleName);
+prompt(lastName);
+
